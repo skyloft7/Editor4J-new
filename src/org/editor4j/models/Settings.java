493cf2d0b3464ea47f5f33cbd62252782c411759
NotThisAgain3 <skyloftmb@gmail.com>
2022-06-27 11:04:29 +0530

    Removed @Serial tag because of Gradle installDist conflicts

--- src/org/editor4j/models/Settings.java
+++ src/org/editor4j/models/Settings.java
@@ -1,11 +1,9 @@
 package org.editor4j.models;
 
 import java.awt.*;
-import java.io.Serial;
 import java.io.Serializable;
 
 public class Settings implements Serializable {
-    @Serial
     private static final long serialVersionUID = 2280736603027290482L;
 
     public Style style;
